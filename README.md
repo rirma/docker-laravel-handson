d2c5ff6ed7f3a3b032a9099535caf20c03098119
rirma <yutakaofvita@gmail.com>
2021-07-29 21:56:27 +0900

    laravel勉強終了

--- README.md
+++ README.md
@@ -3,4 +3,37 @@ laravel勉強
 参考url
 ```
 https://qiita.com/ucan-lab/items/56c9dc3cf2e6762672f4
+https://www.hypertextcandy.com/laravel-tutorial-create-folder/
 ```
+
+sql起動コマンド
+```
+docker-compose exec db bash -c 'mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}'
+```
+
+試し実行コマンド
+```
+php artisan tinker
+```
+
+コントローラー作成
+```
+php artisan make:controller FolderController
+```
+
+モデル作成
+```
+php artisan make:model Folder
+```
+
+バリデーションを行うのはFormRequestクラス
+```
+php artisan make:request CreateFolder
+```
+
+テストコード生成
+```
+php artisan make:test TaskTest
+```
+
+.envは頑張ってネットで拾ってね
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Mail\ResetPassword;
+use Illuminate\Support\Facades\Mail;
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -40,4 +43,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function folders()
+    {
+        return $this->hasMany('App\Models\Folder');
+    }
+
+    /**
+     * ★ パスワード再設定メールを送信する
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        Mail::to($this)->send(new ResetPassword($token));
+    }
 }
